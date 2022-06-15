10d36b4c56aa713c8b036b89a24a9e4a1dc54262
Backheart-js <binhnguyen29122001@gmail.com>
2022-06-15 11:25:58 +0700

    re-build javascript and add function

--- src/js/index.js
+++ src/js/index.js
@@ -3,45 +3,76 @@ const $$ = document.querySelectorAll.bind(document);
 
 const tabs = $$('.category__function-item');
 const panel = $$('.panel__wrapper');
-
-// handle Tab category
-tabs.forEach((tab, index) => {
-    tab.addEventListener('click', function(e) {
-        $('.category__function-item.active').classList.remove('active');
-        this.classList.add('active');
-
-        $('.panel__wrapper.active').classList.remove('active');
-        panel[index].classList.add('active');
-    });
-})
-
-//handle star 
 const stars = $$('.star-icon');
-stars.forEach((star,index) => {
-    let isStar = false;
-    star.addEventListener('click', function(e) {
-        isStar = !isStar;
-        isStar===true ? this.innerText = 'star' : this.innerText = 'star_outline';
-        this.classList.toggle('active', isStar);
-    })
-})
+const labels = $$('.label-icon');
+const dropdown = $('.dropdown-checkbox');
+const line_display = $$('.line-display-wrapper');
 
-//handle checkbox
-// const checkboxAll = $('.table__content-checkbox');
-// const checkbox = $$('.table__content-checkbox');
 
-// checkboxAll.addEventListener('change', function(e) {
-//     console.log(this.checked);
-//     if (this.checked) {
-//         checkbox.forEach((check) => {
-//             console.log(this.checked);
-//             check.checked = 'true';
-//         })
-//     }
-//     else {
-//         checkbox.forEach((check) => {
-//             console.log(this.checked);
-//             check.checked = 'false';
-//         })
-//     }
-// })
+const page = {
+    data: [
+        //Dữ liệu từ API sẽ đổ ra đây
+    ],
+
+    //handle star 
+    handleFuncTable: function(elements) {
+        elements.forEach((element, index) => {
+            let isActive = false;
+            element.addEventListener('click', function (e) {
+                isActive = !isActive;
+                if (elements === stars) {
+                    isActive === true ? this.innerText = 'star' : this.innerText = 'star_outline';
+                }
+                this.classList.toggle('active', isActive);
+            })
+        })
+    },
+    dropdownFunc: function () {
+        dropdown.classList.toggle('active-block');
+    },
+
+    handleEvent: function () {
+        // handle Tab category
+        tabs.forEach((tab, index) => {
+            tab.addEventListener('click', function (e) {
+                $('.category__function-item.active').classList.remove('active');
+                this.classList.add('active');
+
+                $('.panel__wrapper.active').classList.remove('active');
+                panel[index].classList.add('active');
+            });
+        })
+
+        //handle dropdown
+        dropdown.addEventListener('click', function (e) {
+            e.stopPropagation();
+        })
+        window.onclick = function (e) {
+            if (!e.target.matches('.icon-dropdown')) {
+                const dropdownContain = $(".dropdown-checkbox");
+                if (dropdownContain.classList.contains('active-block')) {
+                    dropdownContain.classList.remove('active-block');
+                }
+            }
+        }
+
+        //handle line display
+        line_display.forEach((line,index) => {
+            line.addEventListener('click', function (e) {
+                $('.line-display-wrapper.active').classList.remove('active');
+                this.classList.add('active');
+            })
+        })
+    },
+
+    start: function () {
+        this.handleEvent();
+
+        this.dropdownFunc();
+
+        this.handleFuncTable(stars);
+        this.handleFuncTable(labels);
+    } 
+}
+
+page.start();
