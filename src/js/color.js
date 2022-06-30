def35348ea443cb748b8510a4c27b6603523ff0e
Backheart-js <binhnguyen29122001@gmail.com>
2022-06-30 22:55:58 +0700

    complete create document background

---
+++ src/js/color.js
@@ -0,0 +1,57 @@
+export const colors = [
+    {
+        id: 1,
+        name: 'pink',
+        code: '#FF69B4'
+    },
+    {
+        id: 2,
+        name: 'lavender',
+        code: '#E6E6FA'
+    },
+    {
+        id: 3,
+        name: 'purple',
+        code: '#663399'
+    },
+    {
+        id: 4,
+        name: 'red',
+        code: '#FF0000'
+    },
+    {
+        id: 5,
+        name: 'orange',
+        code: '#FFA500'
+    },
+    {
+        id: 6,
+        name: 'yellow',
+        code: '#FFFF00'
+    },
+    {
+        id: 7,
+        name: 'khaki',
+        code: '#BDB76B'
+    },
+    {
+        id: 8,
+        name: 'green',
+        code: '#008000'
+    },
+    {
+        id: 9,
+        name: 'aqua',
+        code: '#00FFFF'
+    },
+    {
+        id: 10,
+        name: 'blue',
+        code: '#0000FF'
+    },
+    {
+        id: 11,
+        name: 'black',
+        code: '#000'
+    },
+]
--- src/js/modal.js
+++ src/js/modal.js
@@ -1,45 +1,67 @@
+import { colors } from './color.js';
+
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
 const modalContent = $('.modal-container');
+const data = {};
 
 function addElementIntoModal( type ) {
     if ( type === 'addLabel') {
-        console.log('Adding element')
-
-        const html = `
-                <div class="modal-title">
-                    <h3>Thêm nhãn</h3>
-                    <div class="modal-close-btn">
-                        <span class="icon material-icons">
-                        close
-                        </span>
-                    </div>
+        data.title = "Thêm nhãn";
+        data.content = `
+            <div class="">
+                <p>Vui lòng nhập tên nhãn mới</p>
+                <input type="text" class="" placeholder="Enter">
+            </div>
+            <div class="color-choose-wrapper">
+                <span>Chọn màu nhãn:</span>
+                <div class="color-container">
+                    ${colors.map((color, index) => {
+                        return `
+                            <div class="color-choose color-${index+1} flex-center">
+                                <span class="color-choose-icon material-icons">
+                                    done
+                                </span>
+                            </div>
+                        `
+                    })}
                 </div>
-                <div class="modal-body">
-
-                </div>
-            `
-    
-        modalContent.innerHTML = html;
+                <button type="button" class="delete-color">
+                    Xóa màu
+                </button>
+            </div>
+        `;
     }
     else if ( type === 'addPost') {
-        const html = `
-                <div class="modal-title">
-                    <h3>Thêm post</h3>
-                    <div class="modal-close-btn">
-                        <span class="icon material-icons">
-                        close
-                        </span>
-                    </div>
-                </div>
-                <div class="modal-body">
-
-                </div>
-            `
-    
-        modalContent.innerHTML = html;
+        data.title = "Thêm post";
+        data.content = `
+            
+        `;
     }
+
+    const html = `
+            <div class="modal-title">
+                <h3>${data.title}</h3>
+                <div class="modal-close-btn">
+                    <span class="icon material-icons">
+                    close
+                    </span>
+                </div>
+            </div>
+            <div class="modal-body">
+                ${data.content}
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn-white">
+                    Hủy
+                </button>
+                <button type="button" class="btn-primary">
+                    Thêm
+                </button>
+            </div>
+        `
+    modalContent.innerHTML = html;
 }
 
 export default addElementIntoModal;
